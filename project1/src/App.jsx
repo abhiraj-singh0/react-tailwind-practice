0a1c7229ff7e902be524047bb13152baad41ab2f
Abhiraj Singh <singhabhiraj614@gmail.com>
2026-07-18 04:06:09 +0530

    design the navbar

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,8 +1,36 @@
 
 function App() {
   return(
-    <div>
-      <h1 className="bg-[#0e128b]">Hello World</h1>
+
+    <div className="p-0 m-0 h-screen flex flex-col">
+      <nav className="flex justify-around items-center h-18">
+        <div>
+          <img src="/images/brand_logo.png"/>
+        </div>
+        <ul className="flex gap-3">
+          
+          <li><a href="">MENU</a></li>
+          <li><a href="">LOCATION</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">CONATCT</a></li>
+        </ul>
+
+        <div>
+          <a href="" className="h-8 w-18.75 bg-red-600 text-white flex justify-center items-center">Login</a>
+        </div>
+      </nav>
+
+      <div className="w-full flex justify-center items-center h-196">
+        <div className="w-281.25 h-153.25">
+          <div><h1 className="text-7xl font-bold h-76.5 w-148.75">YOUR FEET<br/>
+            DESERVE <br/>
+            THE BEST</h1></div>
+
+          <div>
+            <img src="/images/shoe_image.png"/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
