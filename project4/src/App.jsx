0e0a3fb71ad4734165033bb8d3b71cfd6ee2df7f
Abhiraj Singh <singhabhiraj614@gmail.com>
2026-07-27 20:44:52 +0530

    added some design to boxes

--- project4/src/App.jsx
+++ project4/src/App.jsx
@@ -5,12 +5,12 @@ function App() {
   return(
     <>
     <Home/>
-    <Box title = "BOILED EGG" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "BOILED EGG" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "BOILED EGG" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "BOILED EGG" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "BOILED EGG" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "PANCAKE" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "BOILED EGG" price = "$10.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "BOILED EGG" price = "$14.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "BOILED EGG" price = "$12.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "BOILED EGG" price = "$16.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "BOILED EGG" price = "$18.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "PANCAKE" price = "$8.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
 
     </>
   )
