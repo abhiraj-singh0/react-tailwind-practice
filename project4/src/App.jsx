5d9c1765c0d11025b58c3d00a894d2eb5be8f2dd
Abhiraj Singh <singhabhiraj614@gmail.com>
2026-07-28 20:38:28 +0530

    added some design to boxes

--- project4/src/App.jsx
+++ project4/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <Box title = "BOILED EGG" price = "$10.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
     <Box title = "BOILED EGG" price = "$14.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
     <Box title = "BOILED EGG" price = "$12.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "BOILED EGG" price = "$16.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
-    <Box title = "BOILED EGG" price = "$18.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "CAKE" price = "$16.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
+    <Box title = "BURGER" price = "$18.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
     <Box title = "PANCAKE" price = "$8.00" name = "lorem ipsum dolor sit amet consectetur optio commodi quod distinctio sunt."/>
 
     </>
